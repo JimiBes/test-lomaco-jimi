23ace3ab7a96a9e54a9082d41cd92576f4b0e5c0
JimiBes <jimi.bes@gmail.com>
2024-02-04 10:05:22 +0100

    ajout de la modale de suppression des utilisateurs au clic sur le bouton de suppression et style adapté à celle-ci, le bouton fonctionne même si il ne supprime pas réellement l'utilisateur.

---
+++ src/components/deleteConfirmationModal.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import '../styles/deleteConfirmationModal.scss';
+
+const DeleteConfirmationModal = ({ isOpen, onCancel, onConfirm, userName }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal-container">
+      <div className="delete-confirmation-modal">
+        <p>{`Veuillez confirmer la suppression de l'utilisateur ${userName}`}</p>
+        <div className="delete-buttons">
+          <button onClick={onConfirm}>Valider la suppression</button>
+          <button onClick={onCancel}>Annuler</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DeleteConfirmationModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  userName: PropTypes.string.isRequired,
+};
+
+export default DeleteConfirmationModal;
